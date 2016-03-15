e3ea13c67159d290a2e21fc66706c7955f9e18f7
brannenw2405 <thebranbarian@gmail.com>
2016-03-15 12:19:54 -0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,9 +1,1 @@
-(function() {  
-    var dialog = document.getElementById('window');  
-    document.getElementById('show').onclick = function() {  
-        dialog.show();  
-    };  
-    document.getElementById('exit').onclick = function() {  
-        dialog.close();  
-    };  
-})();
+
